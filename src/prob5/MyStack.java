a79eda948238f07628751e89768480678977aaf2
jungeuidon <mantopuma@naver.com>
2019-08-12 21:30:14 +0900

    5번 수정해야 함

--- src/prob5/MyStack.java
+++ src/prob5/MyStack.java
@@ -1,8 +1,54 @@
 package prob5;
 
-public class MyStack<T> {
+public class MyStack {
 	private int top;
 	private String[] buffer;
-
+	
+	
+	public MyStack(int numb) {
+		this.buffer = new String[numb];
+		top = -1;
+	}
+	
+	public void push(String word) {
+		String[] temp;
+		top++;
+		buffer[top] = word;
+		if(buffer.length<top) {
+			for (int i = 0; i < temp.length; i++) {
+				
+			}
+			
+		}
+	}
+	
+	public String resize(String[] buffer) {
+		String temp[] = new String[buffer.length + 1];
+		for (int i = buffer.length; i >-1; i--) {
+			for (int j = 0; j < temp.length; j++) {
+				temp[j] = buffer[i];
+				String result = temp[j];
+			}
+		}
+		
+		
+		return result;
+	}
+	
+	public String pop() {
+		String result = buffer[top];
+		top--;
+		return result; 
+		
+	}
+	
+	
+	public boolean isEmpty() {
+		if(top==-1) {
+			return true;
+		}else{
+			return false;
+		}
+	}
 	
 }
--- src/prob5/MyStackException.java
+++ src/prob5/MyStackException.java
@@ -10,4 +10,4 @@ public class MyStackException extends Exception {
 	public MyStackException( String message ) {
 		super( message );
 	}
-}
+} 
